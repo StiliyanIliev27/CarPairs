1be344f76e333613edd568de1132b3bd3dd5116c
Stiliyan Iliev <stiliyaniliev2705@gmail.com>
2026-03-03 16:43:53 +0200

    commit changes

---
+++ CarPairs.API/Controllers/ManufacturersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarPairs.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManufacturersController : ControllerBase
+    {
+    }
+}
---
+++ CarPairs.API/DTOs/Parts/PartDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarPairs.API.DTOs.Parts
+{
+    public class PartDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public string ManufacturerName { get; set; } = null!;
+        public string CategoryName { get; set; } = null!;
+        public string CreateAt { get; set; } = null!;
+    } 
+}
